3b917757476a6ebebb32be361d72555e82dbbe1b
sforcloud <32661095+sforcloud@users.noreply.github.com>
2019-10-23 01:17:43 +0530

    Create variables.tf

---
+++ terraform/AWS/variables/variables.tf
@@ -0,0 +1,14 @@
+variable "access_key" {
+description = "AWS access key"
+default = "copy your accesskeys"
+}
+
+variable "secret_key" {
+description = "AWS secret key"
+default = "copy your secretaccess keys"
+}
+
+variable "region" {
+description = "AWS region for hosting our your network"
+default = "us-east-1"
+}
